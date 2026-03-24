79c33d32fa09e0e53b2cfb5f1034bda34a8871ea
wshiqyuan <3453177021@qq.com>
2026-03-24 21:50:54 +0800

    fix(directives): 优化权限指令中的角色匹配逻辑

--- src/directives/permission.ts
+++ src/directives/permission.ts
@@ -1,10 +1,15 @@
 import { useUserStore } from "@/store/auth";
+
+function hasInersection(arr1: Array<any>, arr2: Array<any>) {
+  return arr1.some((item) => arr2.includes(item));
+}
+
 export default {
   beforeMount(el: any, binding: any) {
     const userStore = useUserStore();
     const { roles } = userStore;
     const requiredRoles = binding.value;
-    if (!roles[0].includes(requiredRoles)) {
+    if (!hasInersection(roles, requiredRoles)) {
       el.style.display = "none";
     }
   },
